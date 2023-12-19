8921974c547917bef98f48835182a18b46c1f740
Adrian Covarrubias <adriancova.95@gmail.com>
2023-12-18 18:01:46 -0800

    setting up daisyui and icons library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 import { type Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  // deno-lint-ignore no-explicit-any
+  plugins: [daisyui as any],
+  daisyui: {
+    themes: ["dim"],
+  },
 } satisfies Config;
