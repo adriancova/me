4e9204da3c8938da1647856bdc8162aad4c25889
Adrian Covarrubias <adriancova.95@gmail.com>
2023-12-18 18:20:35 -0800

    Simple footer

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,29 @@
+import Footer from "../components/Footer.tsx";
 import ToDo from "../islands/ToDo.tsx";
 
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto relative">
-      <div class="flex absolute left-4 top-0 items-center">
-        <img
-          class="my-6"
-          src="/logo-transparent.png"
-          width="128"
-          height="128"
-          alt="A logo"
-        />
-        <h1 class="text-4xl font-bold text-white">Howdy!</h1>
-      </div>
+    <>
+      <div class="px-4 pt-8 pb-32 mx-auto relative">
+        <div class="flex absolute left-4 top-0 items-center">
+          <img
+            class="my-6"
+            src="/logo-transparent.png"
+            width="128"
+            height="128"
+            alt="A logo"
+          />
+          <h1 class="text-4xl font-bold text-white">Howdy!</h1>
+        </div>
 
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center mt-24">
-        <h2 class="text-white text-2xl font-bold">
-          Welcome to my website.
-        </h2>
-        <ToDo />
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center mt-24">
+          <h2 class="text-white text-2xl font-bold">
+            Welcome to my website.
+          </h2>
+          <ToDo />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
