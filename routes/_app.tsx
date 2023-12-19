da0728dbeb91a24dcc7b60e8afaf520f93754625
Adrian Covarrubias <adriancova.95@gmail.com>
2023-12-18 17:06:43 -0800

    adding font and favicon to head

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,33 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>adriancova</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+
+        <link rel="icon" type="image/x-icon" href="favicon/favicon.ico"></link>
+        <link rel="manifest" href="favicon/site.webmanifest" />
+        <title>AdrianCova's Website</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap"
+          rel="stylesheet"
+        />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
